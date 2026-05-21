b4d65c98ddb2333c2e558198af77fa3854bc1703
Rehatbir Singh <rehatbir.singh@gmail.com>
2026-05-21 19:36:31 +0400

    feat: improved smiley

--- marc/__main__.py
+++ marc/__main__.py
@@ -1,11 +1,19 @@
 from typing import override
+
 from textual.app import App, ComposeResult
-from textual.widgets import Digits, Footer, Header
+from textual.widgets import Footer, Header, Static
 
 
-class Smiley(Digits):
+class Smiley(Static):
     def __init__(self) -> None:
-        super().__init__("8)")
+        super().__init__(
+            """        ─╮
+╶──╴     │
+     ═   │
+╶──╴     │
+        ─╯
+"""
+        )
 
 
 class MarcApp(App):
