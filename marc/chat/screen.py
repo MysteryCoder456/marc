34d2e96c0134fff72afc14b4dc54edf65740909d
Rehatbir Singh <rehatbir.singh@gmail.com>
2026-06-21 16:07:42 +0400

    feat: tool to search LTM + updated system prompt

--- marc/chat/screen.py
+++ marc/chat/screen.py
@@ -2,7 +2,6 @@ import asyncio
 from typing import final, override
 from uuid import UUID
 
-from anyio import Path
 from langchain_core.messages import (
     AnyMessage,
 )
@@ -16,7 +15,7 @@ from textual.screen import Screen
 from textual.widgets import Footer, Input
 from textual.worker import Worker, WorkerState
 
-from marc.agent import RuntimeContext, create_new_agent
+from marc.agent import RuntimeContext, create_runtime_context, create_new_agent
 from marc.agent.chat_name import generate_chat_name
 from marc.agent.memory import ShortTermMemory
 
@@ -44,6 +43,7 @@ class ChatScreen(Screen):
         self.chat_id = chat_id
         self.is_context_loaded = True
         self.added_messages: set[str] = set()
+        self.agent_runtime: RuntimeContext
 
         self.agent: Runnable
 
@@ -56,6 +56,7 @@ class ChatScreen(Screen):
     async def on_mount(self):
         # Initialize agent
         self.agent = await create_new_agent()
+        self.agent_runtime = await create_runtime_context()
 
         if self.chat_id and (ses := await ChatStorage.load_chat(self.chat_id)):
             # Open existing chat
@@ -154,7 +155,7 @@ class ChatScreen(Screen):
             {"messages": query_messages},
             {"configurable": {"thread_id": self.chat_id}},
             stream_mode="values",
-            context=RuntimeContext(cwd=await Path.cwd()),
+            context=self.agent_runtime,
         )
         async for chunk in response:
             chunk_msgs = chunk["messages"]
