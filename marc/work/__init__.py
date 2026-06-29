c066b3a7b3602657d43f580dcdd426c37b686a99
Rehatbir Singh <rehatbir.singh@gmail.com>
2026-06-29 11:51:07 +0400

    feat: using Monaspice font and drawing face using text

--- marc/work/__init__.py
+++ marc/work/__init__.py
@@ -1,33 +1,25 @@
 # pyright: reportGeneralTypeIssues=false
 
 import time
+from pathlib import Path
 
 import dearpygui.dearpygui as dpg
 from mss import MSS
 
+FONT_PATH = Path(__file__).parent / "MonaspiceKrNerdFont-Regular.otf"
 VIEWPORT_SIZE = (300, 200)
 WHITE: tuple[int, int, int] = (255, 255, 255)
-MUTED: tuple[int, int, int] = (100, 100, 100)
-
-
-def draw_face():
-    with dpg.drawlist(width=150, height=70):
-        with dpg.draw_layer(tag="face"):
-            dpg.draw_line((0, 10), (50, 10), thickness=3)
-            dpg.draw_line((0, 60), (50, 60), thickness=3)
-            dpg.draw_bezier_quadratic(
-                (100, 0), (150, 35), (100, 70), thickness=3
-            )
+MUTED: tuple[int, int, int] = (120, 120, 120)
 
 
 def main():
+    # Setup
     with MSS() as sct:
         mon = sct.monitors[1]
         viewport_position = (
             mon["width"] - VIEWPORT_SIZE[0],
             0,
         )
-
     dpg.create_context()
     dpg.create_viewport(
         title="Work Mode",
@@ -35,47 +27,55 @@ def main():
         y_pos=viewport_position[1],
         width=VIEWPORT_SIZE[0],
         height=VIEWPORT_SIZE[1],
-        # decorated=False,
         always_on_top=True,
+        # decorated=False,
     )
 
+    # Register fonts
+    with dpg.font_registry():
+        dpg.add_font(str(FONT_PATH), 16, tag="font_md")
+        dpg.add_font(str(FONT_PATH), 72, tag="font_face")
+
+        # Global default font
+        dpg.bind_font("font_md")
+
+        # Enable nerd fonts
+        dpg.add_font_range(0xE000, 0xF8FF)
+
+    indicator_frame = 0
+    frame_elapsed = 0
+    frame_duration = 0.1
+    frames = ""
+
+    # Build UI
     with dpg.window(tag="Work Mode"):
         with dpg.group():
-            draw_face()
+            dpg.add_text("=D", tag="face")
+            dpg.bind_item_font("face", "font_face")
 
             with dpg.group(horizontal=True):
                 dpg.add_text("Discombobulating", color=MUTED)
+                dpg.add_text(frames[0], color=MUTED, tag="indicator")
 
-                for i in range(3):
-                    dpg.add_text("·", color=MUTED, tag=f"indicator-{i}")
-
+    # More setup
     dpg.setup_dearpygui()
     dpg.show_viewport()
     dpg.set_primary_window("Work Mode", True)
 
-    indicator_frame = 0
-    frame_elapsed = 0
-
+    # Render loop
     prev_now = time.time()
     while dpg.is_dearpygui_running():
         now = time.time()
         dt = now - prev_now
 
         frame_elapsed += dt
-        if frame_elapsed > 0.1:
-            frame_elapsed = 0
-
-            if indicator_frame >= 0:
-                dpg.configure_item(f"indicator-{indicator_frame}", color=MUTED)
-
-            indicator_frame += 1
-            if indicator_frame >= 3:
-                frame_elapsed = -4 * 0.1
-                indicator_frame = -1
-            else:
-                dpg.configure_item(f"indicator-{indicator_frame}", color=WHITE)
+        if frame_elapsed > frame_duration:
+            frame_elapsed -= frame_duration
+            indicator_frame = (indicator_frame + 1) % len(frames)
+            dpg.set_value("indicator", frames[indicator_frame])
 
         dpg.render_dearpygui_frame()
         prev_now = now
 
+    # Cleanup
     dpg.destroy_context()
