9abd7b8ef168602635fbb21431245096c7808890
Rehatbir Singh <rehatbir.singh@gmail.com>
2026-07-14 13:26:11 +0400

    fix: massively improved tool call visibility

--- marc/chat/message.py
+++ marc/chat/message.py
@@ -1,13 +1,57 @@
 from typing import final, override
 
-from langchain_core.messages import AnyMessage
+from langchain_core.messages import AnyMessage, ToolCall, ToolMessage
 from textual.app import ComposeResult
 from textual.containers import VerticalGroup
-from textual.widgets import Markdown, Pretty, Static
+from textual.reactive import reactive
+from textual.widget import Widget
+from textual.widgets import Markdown, Pretty, Static, Tree
 
 
 @final
-class ChatMessage(Static):
+class ToolCallBlock(Widget):
+    tool_result: reactive[ToolMessage | None] = reactive(None, recompose=True)
+
+    def __init__(self, tool_call: ToolCall):
+        super().__init__(
+            id=f"tool-call-{tool_call['id']}", classes="tool-call"
+        )
+        self.tool_call = tool_call
+
+    @property
+    def tool_call_repr(self) -> str:
+        args_str = ", ".join(
+            f"{k}={v}" for k, v in self.tool_call["args"].items()
+        )
+        return f"{self.tool_call['name']}({args_str})"
+
+    @property
+    def tool_result_repr(self) -> list[str]:
+        if not self.tool_result:
+            return ["[dim i]Waiting for result...[/]"]
+
+        output: list[str] = []
+        for block in self.tool_result.content_blocks:
+            block_repr: str
+            if block["type"] == "text":
+                block_repr = block["text"]
+            else:
+                block_repr = f"[dim][{block['type'].capitalize()} output][/]"
+            output.append(block_repr)
+        return output
+
+    @override
+    def compose(self) -> ComposeResult:
+        t = Tree(self.tool_call_repr)
+
+        for block_repr in self.tool_result_repr:
+            t.root.add_leaf(block_repr)
+
+        yield t
+
+
+@final
+class ChatMessageBlock(Static):
     TITLES = {
         "ai": "Marc",
     }
@@ -36,21 +80,15 @@ class ChatMessage(Static):
                     case "text":
                         yield Markdown(block["text"])
 
-                    case "tool_call":
-                        args_str = ", ".join(
-                            f"{k}={v}" for k, v in block["args"].items()
-                        )
-                        yield Static(
-                            f"[dim italic $text-accent]Tool Call: {block['name']}({args_str})[/]"
-                        )
-
-                    case "image":
-                        mime_type = block.get("mime_type", "unknown")
-                        yield Static(f"[dim italic]Image: type={mime_type}[/]")
-
                     case "reasoning":
                         reasoning = block.get("reasoning", "No reasoning")
-                        yield Static(f"[dim]{reasoning}[/]")
+                        yield Markdown(reasoning, classes="reasoning-block")
+
+                    case "tool_call":
+                        pass
+
+                    case "image":
+                        pass
 
                     case _:
                         self.log(block)
